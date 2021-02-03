a12ad716394a8fa72d07177d70c82566b6453035
ErrorMi <error.mititch@gmail.com>
2021-02-04 01:21:40 +0300

    Create whol program

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let phrases = {
+let data = {
 	nameOfProduct: ['Балансер', 'Леденцы', 'Тряпочка из микрофибры с ионами серебра',
 					'Швабра', 'Икона святой богородицы', 'Витаминки', 'Эфирное масло - тонус',
 					'Ультра новая тряпочка из микрофибры(уже синего цвета)', 'Леденцы со вкусом говна',
@@ -8,3 +8,16 @@ let phrases = {
 					'кушать какашки', 'нюхать подмышки',
 					'ходить в общественный туолет']
 };
+
+
+let nameOfProduct = data.nameOfProduct[Math.floor(Math.random() * data.nameOfProduct.length)];
+let firstPath = 'Привет я Ира и я хочу представить вам новый продукт от Green Way: ' + nameOfProduct;
+
+let whatItCanDo = data.whatItCanDo[Math.floor(Math.random() * data.whatItCanDo.length)];
+let secondPath = 'С его помощью вы сможете ' + whatItCanDo;
+
+let discount = 70 + Math.floor(Math.random() * 25)
+let thirdPath = `Срочно покупайте! ТОЛЬКО СЕГОДНЯ НА НЕГО СКИДКА ${discount}%`;
+
+let message = `${firstPath}\n${secondPath}\n${thirdPath}`
+console.log(message)
